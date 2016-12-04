a0d3e146188f0499c313d12b08d6bf8936d9b2f4
masmix1 <gabriel.wawerski@gmail.com>
2016-12-04 23:00:52 +0100

    added fpsLogger, camera, viewport, player atlas, played around with player animations, added clearcolor, clear in render() added update call in resize() added tiles.pack/png, added Map, Tile classes

---
+++ core/src/com/masmix/gra/Map.java
@@ -0,0 +1,7 @@
+package com.masmix.gra;
+
+/*
+ * Created by MasmiX on 04.12.2016.
+ */
+public class Map {
+}
---
+++ core/src/com/masmix/gra/Tile.java
@@ -0,0 +1,10 @@
+package com.masmix.gra;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/*
+ * Created by MasmiX on 04.12.2016.
+ */
+public class Tile {
+    private TextureRegion tileTexture;
+}
