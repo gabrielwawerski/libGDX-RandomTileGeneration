5095bdae8a24630a46b9c8b6b8504e4f4d39c9db
masmix1 <gabriel.wawerski@gmail.com>
2016-12-05 00:16:36 +0100

    working on tile class added tileID, posX, posY, tiletexture, constructor, getters, setter static ID for tiles, every tile gets an unique tileID

--- core/src/com/masmix/gra/Tile.java
+++ core/src/com/masmix/gra/Tile.java
@@ -6,5 +6,50 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  * Created by MasmiX on 04.12.2016.
  */
 public class Tile {
+    private int posX;
+    private int posY;
+    private static int ID = 0;
+    private int tileID;
     private TextureRegion tileTexture;
+
+
+    public Tile(int posX, int posY, TextureRegion tileTexture) {
+        ID += 1;
+        this.tileID = ID;
+        this.posX = posX;
+        this.posY = posY;
+        this.tileTexture = tileTexture;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getTileID() {
+        return tileID;
+    }
+
+    public void setTileID(int tileID) {
+        this.tileID = tileID;
+    }
+
+    public TextureRegion getTileTexture() {
+        return tileTexture;
+    }
+
+    public void setTileTexture(TextureRegion tileTexture) {
+        this.tileTexture = tileTexture;
+    }
 }
